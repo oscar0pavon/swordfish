22c5fbb0d83a239d1d63d046700f1701cba6bc50
Oscar Pavon <pavon_oscar@outlook.com>
2025-10-22 02:31:29 -0300

    translate and rotate secondary cube

--- source_code/swordfish.c
+++ source_code/swordfish.c
@@ -2,8 +2,12 @@
 #include "engine/array.h"
 #include <engine/model.h>
 
+#include "renderer/cglm/affine.h"
+#include "renderer/cglm/mat4.h"
+#include "renderer/cglm/types.h"
 #include "renderer/pipeline.h"
 #include <engine/engine2d.h>
+#include <stdint.h>
 #include <stdio.h>
 
 #include "renderer/cglm/cglm.h"
@@ -22,6 +26,38 @@ Camera main_camera;
 
 bool finished_build = false;
 
+float left_position_z = -2;
+
+void init_secodary_cube(PModel* model){
+   
+  mat4 identity;
+  glm_mat4_identity(identity);
+  glm_translate(identity, VEC3(0,1,-1));
+  glm_translate(identity, VEC3(0,0,-2));
+
+  glm_mat4_copy(identity, model->model_mat);
+  
+}
+
+void sworfish_set_secondary_cube_position(PModel* model, uint32_t image_index){
+
+  //this is the far the cube can reach
+  float velocity = 0.001f;
+  if(left_position_z < -0.1){
+
+    glm_translate(model->model_mat, VEC3(0,0,velocity));
+    left_position_z += velocity;
+  }
+  mat4 final_pisition;
+  glm_mat4_mul(main_cube.model_mat, model->model_mat, final_pisition);
+
+  
+  //glm_mat4_copy(model->model_mat, model->uniform_buffer_object.model);
+  glm_mat4_copy(final_pisition, model->uniform_buffer_object.model);
+
+  pe_vk_send_uniform_buffer(model, image_index);
+}
+
 void swordfish_draw_scene(VkCommandBuffer *cmd_buffer, uint32_t index){
 
   //main cube
@@ -37,7 +73,7 @@ void swordfish_draw_scene(VkCommandBuffer *cmd_buffer, uint32_t index){
   pe_vk_draw_model(&draw_cube);
 
   //secondary_cube
-  swordfish_update_main_cube(&secondary_cube, index);
+  sworfish_set_secondary_cube_position(&secondary_cube, index);
   pe_vk_descriptor_update(&secondary_cube);
 
   PDrawModelCommand draw_seconday_cube = {
@@ -63,6 +99,7 @@ void swordfish_draw_scene(VkCommandBuffer *cmd_buffer, uint32_t index){
   pe_vk_draw_model(&draw_quad);
 
 }
+
 
 //INFO this where you update the position of the model or camera
 void swordfish_update_main_cube(PModel *model, uint32_t image_index) {
@@ -104,6 +141,7 @@ void swordfish_init(){
 
   secondary_cube.pipeline = main_cube.pipeline;
 
+  init_secodary_cube(&secondary_cube);
 
   pe_2d_init();
   pe_2d_create_quad(&quad_model,1,1,1,1);
