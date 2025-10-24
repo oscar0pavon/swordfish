21be9f694f8fb5763b2f28e2f612e94595319270
Oscar Pavon <pavon_oscar@outlook.com>
2025-10-24 03:30:41 -0300

    get monitor crtcs

--- source_code/direct_render.c
+++ source_code/direct_render.c
@@ -7,6 +7,8 @@
 #include <xf86drm.h>
 #include <xf86drmMode.h>
 
+#include <engine/numbers.h>
+
 #define DRM_DEVICE_PATH "/dev/dri/card0"
 
 
@@ -15,7 +17,73 @@ typedef struct KernelModeSettingDevice{
     drmModeRes* resources;
 }KernelModeSettingDevice;
 
+typedef struct PMonitor{
+  drmModeConnector *connector;
+  drmModeEncoder *encoder;
+  drmModeCrtc *crtc;
+}PMonitor;
+
 KernelModeSettingDevice drm_device;
+
+#define MAX_MONITOR 8
+PMonitor monitors[MAX_MONITOR] = {};
+u8 monitors_number = 0;
+
+void get_drm_info() {
+
+  int connector_number;
+  int encoder_number;
+
+
+  drmModeConnector *current_connector;
+  drmModeEncoder *current_encoder;
+  drmModeCrtc *current_crtc;
+
+  for (connector_number = 0;
+       connector_number < drm_device.resources->count_connectors;
+       connector_number++) {
+    
+    current_connector =
+        drmModeGetConnector(drm_device.file_descriptor,
+                            drm_device.resources->connectors[connector_number]);
+    
+    if(current_connector->connection == DRM_MODE_CONNECTED &&
+        current_connector->count_modes > 0) {
+      printf("Monitor connected %i\n",connector_number);
+
+      for (encoder_number = 0;
+           encoder_number < current_connector->count_encoders;
+           encoder_number++) {
+        
+        current_encoder =
+            drmModeGetEncoder(drm_device.file_descriptor,
+                              current_connector->encoders[encoder_number]);
+
+        //CRTC - Cathode Ray Tube Controller 
+        if(current_encoder->crtc_id){
+          current_crtc = drmModeGetCrtc(drm_device.file_descriptor,
+                                        current_encoder->crtc_id);
+          
+          monitors[connector_number].connector = current_connector;
+          monitors[connector_number].encoder = current_encoder;
+          monitors[connector_number].crtc = current_crtc;
+          monitors_number++;
+
+          continue; // we don't free conector and encoder if we find a monitor
+
+
+        }
+
+
+        printf("Free encoder\n");
+        drmModeFreeEncoder(current_encoder);
+      }
+    }
+
+    printf("Free connector %i\n",connector_number);
+    drmModeFreeConnector(current_connector);
+  }
+}
 
 void init_direct_render(void) {
 
@@ -37,14 +105,22 @@ void init_direct_render(void) {
   }
 
   drm_device.resources = drmModeGetResources(drm_device.file_descriptor);
+  if (!drm_device.resources) {
+    fprintf(stderr, "Failed to get DRM resources: %m\n");
+    close(drm_device.file_descriptor);
+  }
 
-  
+  get_drm_info();
 
 
   clean_drm();
 }
 
 void clean_drm(){
+  for(int i = 0; i<monitors_number; i++){
+    drmModeFreeEncoder(monitors[i].encoder);
+    drmModeFreeCrtc(monitors[i].crtc);
+  }
   drmModeFreeResources(drm_device.resources);
   drmDropMaster(drm_device.file_descriptor);
   close(drm_device.file_descriptor);//otherwise we can't backto our window manager
