09f98651cab7a1829cc000c9191ea1e92c604e09
Oscar Pavon <pavon_oscar@outlook.com>
2025-10-24 10:29:36 -0300

    create display surface

--- source_code/renderer/display.c
+++ source_code/renderer/display.c
@@ -1,7 +1,29 @@
 #include "display.h"
 
 #include "vulkan.h"
+#include "window.h"
 #include <stdio.h>
+#include <vulkan/vulkan_core.h>
+
+VkDisplayKHR vk_display;
+VkDisplayModeKHR vk_display_mode;
+
+void vk_create_display_surface(){
+  VkDisplaySurfaceCreateInfoKHR info = {
+    .sType = VK_STRUCTURE_TYPE_DISPLAY_SURFACE_CREATE_INFO_KHR, 
+    .displayMode = vk_display_mode,
+    .planeIndex = 0,
+    .globalAlpha = 1.0f,
+    .alphaMode = VK_DISPLAY_PLANE_ALPHA_OPAQUE_BIT_KHR,
+    .planeStackIndex = 0, 
+    .imageExtent = {1920,1080},
+    .transform = VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR
+  };
+
+  VKVALID(vkCreateDisplayPlaneSurfaceKHR(vk_instance,
+      &info, NULL, &vk_surface), "Can't create display surface");
+  
+}
 
 void vk_get_displays() {
   u32 display_count;
@@ -10,4 +32,26 @@ void vk_get_displays() {
                                           NULL);
 
   printf("Vulkan displays count: %i\n", display_count);
+
+  VkDisplayPropertiesKHR displays[display_count];
+  vkGetPhysicalDeviceDisplayPropertiesKHR(vk_physical_device,
+      &display_count,
+      displays);
+  
+  vk_display = displays[0].display;
+
+  u32 display_modes_count;
+  vkGetDisplayModePropertiesKHR(vk_physical_device, vk_display, 
+     &display_modes_count,NULL );
+  
+  printf("Display modes count %i\n", display_modes_count);
+
+  VkDisplayModePropertiesKHR modes[display_modes_count];
+  vkGetDisplayModePropertiesKHR(vk_physical_device, vk_display, 
+     &display_modes_count,modes);
+
+  vk_display_mode = modes[0].displayMode;
+  
+  vk_create_display_surface();
+
 }
